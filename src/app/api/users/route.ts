354efe84b5ad710af4270dd8cee1cabe17503443
rodrigovm10 <rv0611200369@gmail.com>
2024-02-29 11:51:14 -0600

    Deploy errors

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -13,3 +13,26 @@ export async function GET() {
     throw error
   }
 }
+// import { db } from '@/server/db/db'
+// import { followAndUnfollowUserSchema } from '@/server/schemas'
+
+// export async function POST(request: Request) {
+//   try {
+//     // TODO: Obtener el usuario actual y el usuario a actualizar
+//     // TODO: Actualizar el usuario actual con el usuario actualizado seguido
+//     // TODO: Actualizar el usuario seguido con el usuario que lo siguio
+//     const body = await request.json()
+//     const result = followAndUnfollowUserSchema.safeParse(body)
+
+//     const { id } = body
+
+//     // const user = await db.user.update({
+//     //   where: { id },
+//     //   data: {
+//     //     : {
+
+//     //     }
+//     //   }
+//     // })
+//   } catch (error) {}
+// }
